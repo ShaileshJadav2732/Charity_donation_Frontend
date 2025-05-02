f8ffc928a3f2825a3490686e47af176e387a8e7c
Shailesh <jshailesh798@gmail.com>
2025-05-02 18:29:13 +0530

    initial project setup with Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className="flex items-center justify-center min-h-screen text-2xl">
+			Welcome to the Charity Donation Platform!
+		</main>
+	);
+}
